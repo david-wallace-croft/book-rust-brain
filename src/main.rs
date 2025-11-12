ab95df7cf7a23412729f8c908462fa274fcfa716
David Wallace Croft <david@CroftSoft.com>
2025-11-11 18:54:28 -0600

    p2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::io::stdin;
+
 fn main() {
+    println!("What is 3 + 2? Type your answer and press enter.");
+
+    let mut input: String = String::new();
+
+    stdin()
+        .read_line(&mut input)
+        .expect("Unable to read standard input");
+
+    if input == "5" {
+        println!("Correct!");
+    } else {
+        println!("Incorrect!");
+    }
+}
+
+fn p1() {
     const F32: f32 = 3.4028236;
 
     println!("{F32}");
