11fd8d047c88cccce2c3a2983807fbc6fa7bfa8d
David Wallace Croft <david@CroftSoft.com>
2026-02-12 21:32:05 -0600

    try_into

--- puzzle08/src/main.rs
+++ puzzle08/src/main.rs
@@ -1,3 +1,5 @@
+// use ::std::num::TryFromIntError;
+
 fn double_it(
   n: u64,
   _: i32,
@@ -8,7 +10,19 @@ fn double_it(
 fn main() {
   let one: i32 = 1;
 
-  let n = double_it(one as _, 3);
+  let n: u64 = double_it(one as _, 3);
 
   println!("{}", n);
+
+  // let n: u64 = double_it(one.into(), 3);
+
+  // println!("{}", n);
+
+  let one_converted_result = one.try_into();
+
+  if let Ok(one_converted) = one_converted_result {
+    let n: u64 = double_it(one_converted, 3);
+
+    println!("{}", n);
+  }
 }
