b8695a48d6633307186d98cfd5ee957aa52eb989
David Wallace Croft <david@CroftSoft.com>
2026-02-24 18:20:19 -0600

    puzzle17

---
+++ puzzle17/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let mut my_vec = Vec::with_capacity(1);
+
+  my_vec.push("Hello");
+
+  println!("{}", my_vec.capacity());
+
+  my_vec.push("World");
+
+  println!("{}", my_vec.capacity());
+}
