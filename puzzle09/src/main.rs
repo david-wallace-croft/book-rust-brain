f1ae1ca461efd9140ecc5462a0ce312dd510bd25
David Wallace Croft <david@CroftSoft.com>
2026-03-24 07:10:35 -0500

    type assertions

--- puzzle09/src/main.rs
+++ puzzle09/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-  let mut floats = vec![
+  let mut floats: Vec<f64> = vec![
     3.1, 1.2, 4.5, 0.3,
   ];
 
   // floats.sort();
 
-  floats.sort_by(|a, b| a.partial_cmp(b).unwrap());
+  floats.sort_by(|a: &f64, b: &f64| a.partial_cmp(b).unwrap());
 
   println!("{:#?}", floats);
 
@@ -17,7 +17,7 @@ fn main() {
     f64::NAN,
   ];
 
-  floats2.sort_by(|a, b| a.total_cmp(b));
+  floats2.sort_by(|a: &f64, b: &f64| a.total_cmp(b));
 
   println!("{:#?}", floats2);
 }
