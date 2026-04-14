41ac5fcf53189da91b7ccd7d47c5a5a72c8021c0
David Wallace Croft <david@CroftSoft.com>
2026-04-14 18:24:54 -0500

    unicode escape X

--- puzzle10/src/main.rs
+++ puzzle10/src/main.rs
@@ -5,6 +5,15 @@ fn main() {
     println!("It does not match.");
   }
 
+  // The capital letter X as a Unicode escape
+  println!("\u{0058}");
+
   // The Greek capital letter Chi as a Unicode escape
   println!("\u{03A7}");
+
+  if 'X' == '\u{58}' {
+    println!("It matches!");
+  } else {
+    println!("It does not match.");
+  }
 }
