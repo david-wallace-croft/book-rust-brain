3e736cc4d732e8041c44c83905170ce83d8a5618
David Wallace Croft <david@CroftSoft.com>
2026-07-15 20:45:20 -0500

    trait Add

--- puzzle16/src/main.rs
+++ puzzle16/src/main.rs
@@ -1,11 +1,32 @@
-fn double_it(n: i32) -> i32 {
-  n * 2
+fn main() {
+  println!("2 * 4 = {}", double_it(4));
 }
+
+// fn double_it(n: i32) -> i32 {
+//   n * 2
+// }
 
 // fn double_it(n: f32) -> f32 {
 //   n * 2.0
 // }
 
-fn main() {
-  println!("2 * 4 = {}", double_it(4));
+// fn double_it<T>(n: T) -> T
+// where
+//   T: ::std::ops::Mul<Output = T> + From<i32>,
+// {
+//   n * 2.into()
+// }
+
+// fn double_it<T>(n: T) -> T
+// where
+//   T: ::std::ops::Add<Output = T> + Copy,
+// {
+//   n.add(n)
+// }
+
+fn double_it<T>(n: T) -> T
+where
+  T: ::std::ops::Add<Output = T> + Clone,
+{
+  n.clone().add(n)
 }
