e11b49336f263a56ae7db322df07248270e90b74
David Wallace Croft <david@CroftSoft.com>
2026-07-28 18:03:25 -0500

    extend

--- puzzle17/src/main.rs
+++ puzzle17/src/main.rs
@@ -1,11 +1,25 @@
 fn main() {
   let mut my_vec = Vec::with_capacity(1);
 
-  my_vec.push("Hello");
+  my_vec.push("1");
 
   println!("{}", my_vec.capacity());
 
-  my_vec.push("World");
+  my_vec.push("2");
 
   println!("{}", my_vec.capacity());
+
+  // for s in vec![
+  //   "3", "4", "5", "6", "7",
+  // ] {
+  //   my_vec.push(s);
+  // }
+
+  // println!("{}", my_vec.capacity());
+
+  // my_vec.extend(vec![
+  //   "3", "4", "5", "6", "7",
+  // ]);
+
+  // println!("{}", my_vec.capacity());
 }
