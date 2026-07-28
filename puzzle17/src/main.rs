ba0993d26611e9a26cd6ef41e5ccad2b830e8ae0
David Wallace Croft <david@CroftSoft.com>
2026-07-28 18:10:26 -0500

    push 9

--- puzzle17/src/main.rs
+++ puzzle17/src/main.rs
@@ -1,21 +1,23 @@
 fn main() {
   let mut my_vec = Vec::with_capacity(1);
 
+  println!("capacity afore pushing: {}", my_vec.capacity());
+
   my_vec.push("1");
 
-  println!("{}", my_vec.capacity());
+  println!("capacity after push(1): {}", my_vec.capacity());
 
   my_vec.push("2");
 
-  println!("{}", my_vec.capacity());
+  println!("capacity after push(2): {}", my_vec.capacity());
 
-  // for s in vec![
-  //   "3", "4", "5", "6", "7",
-  // ] {
-  //   my_vec.push(s);
-  // }
+  for s in vec![
+    "3", "4", "5", "6", "7", "8", "9",
+  ] {
+    my_vec.push(s);
 
-  // println!("{}", my_vec.capacity());
+    println!("capacity after push({s}): {}", my_vec.capacity());
+  }
 
   // my_vec.extend(vec![
   //   "3", "4", "5", "6", "7",
