2c2a75a194c5253cc2db70709eb441d3bf2ba772
David Wallace Croft <david@CroftSoft.com>
2026-08-31 18:40:04 -0500

    steps

--- puzzle20/src/main.rs
+++ puzzle20/src/main.rs
@@ -6,3 +6,27 @@ fn main() {
 
   hello();
 }
+
+#[expect(dead_code)]
+enum Language {
+  English,
+  French,
+}
+
+// This static dispatch const fn executes at compile-time
+#[expect(dead_code)]
+const fn hello_const(language: Language) -> &'static str {
+  match language {
+    Language::English => "Hello, World!",
+    Language::French => "Bonjour le monde",
+  }
+}
+
+// This dynamic dispatch fn executes at run-time
+#[expect(dead_code)]
+fn hello_dynamic(language: Language) {
+  match language {
+    Language::English => || println!("Hello, World!"),
+    Language::French => || println!("Bonjour le monde"),
+  };
+}
